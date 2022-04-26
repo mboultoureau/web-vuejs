c5e2b12e3ed1efc83099b77552e98e9f71a391b3
mboultoureau <contact@mboultoureau.com>
2022-04-26 22:06:21 +0200

    Ajout du nb de livres

--- src/components/pages/AppStatistics.vue
+++ src/components/pages/AppStatistics.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="bg-white shadow overflow-hidden sm:rounded-lg">
     <div class="px-4 py-5 sm:px-6">
-      <h3 class="text-lg leading-6 font-medium text-gray-900">
-        Statistiques
-      </h3>
+      <h3 class="text-lg leading-6 font-medium text-gray-900">Statistiques</h3>
       <p class="mt-1 max-w-2xl text-sm text-gray-500">
         Quelques nombres sur les sorts
       </p>
@@ -22,7 +20,21 @@
           <dt class="text-sm font-medium text-gray-500">Ecoles</dt>
           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
             <ul>
-              <li v-for="(value, key) in schoolsCount" :key="key">{{ key }} : {{ value }}</li>              
+              <li v-for="(value, key) in schoolsCount" :key="key">
+                {{ key }} : {{ value }}
+              </li>
+            </ul>
+          </dd>
+        </div>
+        <div
+          class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+        >
+          <dt class="text-sm font-medium text-gray-500">Livres</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <ul>
+              <li v-for="(value, key) in livresCount" :key="key">
+                {{ key }} : {{ value }}
+              </li>
             </ul>
           </dd>
         </div>
@@ -38,7 +50,7 @@ export default {
   computed: {
     schoolsCount() {
       let schools = {};
-      this.spells.forEach(spell => {
+      this.spells.forEach((spell) => {
         if (schools[spell[2]] != undefined) {
           schools[spell[2]] = schools[spell[2]] + 1;
         } else {
@@ -47,7 +59,20 @@ export default {
       });
 
       return schools;
-    }
-  }
+    },
+    livresCount() {
+      let livres = {};
+
+      this.spells.forEach((spell) => {
+        if (livres[spell[0]] != undefined) {
+          livres[spell[0]] = livres[spell[0]] + 1;
+        } else {
+          livres[spell[0]] = 1;
+        }
+      });
+
+      return livres;
+    },
+  },
 };
 </script>
